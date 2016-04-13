e9dce6c1a39f1c9f261e5301a7fea67a88560b96
Robin Holzwarth <rholzwar@stud.hs-heilbronn.de>
2016-04-14 00:44:39 +0200

    --

---
+++ scripts.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Robin Holzwarth on 13.04.2016.
+ */
+
+var map;
+
+function initMap() {
+    var mapDiv = document.getElementById('map');
+    map = new google.maps.Map(mapDiv, {
+        center: {lat: 49.122631, lng: 9.206131},
+        zoom:24,
+        tilt:0,
+        mapTypeId: google.maps.MapTypeId.SATELLITE
+
+    });
+}
+
+function alterMap(la,ln){
+
+    map.panTo({lat:la,lng:ln});
+
+}
